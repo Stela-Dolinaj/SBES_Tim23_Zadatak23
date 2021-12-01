40e32e8a0ed9e3ce34330c4ce62b9c143de31621
Stela <steladolinaj@gmail.com>
2021-12-01 11:38:34 +0100

    Added Client, Server, Manager and Contracts code from the last class

--- ProjektniZadatak/Client/Program.cs
+++ ProjektniZadatak/Client/Program.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿using Manager;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Security.Principal;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Client
 {
@@ -6,7 +14,54 @@ namespace Client
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /// Define the expected service certificate. It is required to establish cmmunication using certificates.
+            string srvCertCN = "wcfservice";
+
+            /// Define the expected certificate for signing ("<username>_sign" is the expected subject name).
+            /// .NET WindowsIdentity class provides information about Windows user running the given process
+            string signCertCN = Formatter.ParseName(WindowsIdentity.GetCurrent().Name) + "_sign";
+
+            /// Define subjectName for certificate used for signing which is not as expected by the service
+            string wrongCertCN = "wrong_sign";
+
+            NetTcpBinding binding = new NetTcpBinding();
+            binding.Security.Transport.ClientCredentialType = TcpClientCredentialType.Certificate;
+
+            /// Use CertManager class to obtain the certificate based on the "srvCertCN" representing the expected service identity.
+            X509Certificate2 srvCert = CertManager.GetCertificateFromStorage(StoreName.TrustedPeople,
+                StoreLocation.LocalMachine, srvCertCN);
+            EndpointAddress address = new EndpointAddress(new Uri("net.tcp://localhost:9999/Receiver"),
+                                      new X509CertificateEndpointIdentity(srvCert));
+
+            using (WCFClient proxy = new WCFClient(binding, address))
+            {
+                /// 1. Communication test
+                proxy.TestCommunication();
+                Console.WriteLine("TestCommunication() finished. Press <enter> to continue ...");
+                Console.ReadLine();
+
+                /// 2. Digital Signing test	
+				string message = "Message";
+
+                /// Create a signature based on the "signCertCN" using SHA1 hash algorithm
+                Console.WriteLine("Created sign");
+                X509Certificate2 signCert = CertManager.GetCertificateFromStorage(StoreName.My, StoreLocation.LocalMachine, signCertCN);
+                byte[] sign = DigitalSignature.Create(message, HashAlgorithm.SHA1, signCert);
+
+                /// For the same message, create a signature based on the "wrongCertCN" using SHA1 hash algorithm
+                Console.WriteLine("Created wrong sign");
+                X509Certificate2 wrongSignCert = CertManager.GetCertificateFromStorage(StoreName.My, StoreLocation.LocalMachine, wrongCertCN);
+                byte[] wrongSign = DigitalSignature.Create(message, HashAlgorithm.SHA1, wrongSignCert);
+
+                Console.WriteLine("Sent message with sign");
+                proxy.SendMessage(message, sign);
+
+                Console.WriteLine("Sent message with wrong sign");
+                proxy.SendMessage(message, wrongSign);
+
+                Console.ReadLine();
+
+            }
         }
     }
 }
---
+++ ProjektniZadatak/Manager/DigitalSignature.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Security.Cryptography.X509Certificates;
+using System.Security.Cryptography;
+
+namespace Manager
+{
+    public enum HashAlgorithm { SHA1, SHA256}
+	public class DigitalSignature
+	{
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="message"> a message/text to be digitally signed </param>
+		/// <param name="hashAlgorithm"> an arbitrary hash algorithm </param>
+		/// <param name="certificate"> certificate of a user who creates a signature </param>
+		/// <returns> byte array representing a digital signature for the given message </returns>
+		public static byte[] Create(string message, HashAlgorithm hashAlgorithm, X509Certificate2 certificate)
+		{
+            //TO DO
+
+            /// Looks for the certificate's private key to sign a message
+            RSACryptoServiceProvider csp = certificate.PrivateKey as RSACryptoServiceProvider;
+
+            if (csp == null)
+            {
+                throw new Exception("Valid certificate not found");
+            }
+
+            UnicodeEncoding encoding = new UnicodeEncoding();
+            byte[] messageByte = encoding.GetBytes(message);
+
+            /// Use RSACryptoServiceProvider support to create a signature using a previously created hash value
+            byte[] hash = null;
+            if (HashAlgorithm.SHA1 == hashAlgorithm)
+            {
+                SHA1Managed sha1 = new SHA1Managed();
+                hash = sha1.ComputeHash(messageByte);
+            }
+            else if (hashAlgorithm == HashAlgorithm.SHA256)
+            {
+                SHA256Managed sha256 = new SHA256Managed();
+                hash = sha256.ComputeHash(messageByte);
+            }
+
+
+            byte[] signature = csp.SignHash(hash, CryptoConfig.MapNameToOID(hashAlgorithm.ToString()));           
+
+            return signature;
+        }
+
+
+		public static bool Verify(string message, HashAlgorithm hashAlgorithm, byte[] signature, X509Certificate2 certificate)
+		{
+            //TO DO
+
+            /// Looks for the certificate's public key to sign a message
+            RSACryptoServiceProvider csp = certificate.PublicKey.Key as RSACryptoServiceProvider;
+
+            UnicodeEncoding encoding = new UnicodeEncoding();
+            byte[] messageByte = encoding.GetBytes(message);
+
+            byte[] hash = null;
+            if (HashAlgorithm.SHA1 == hashAlgorithm)
+            {
+                SHA1Managed sha1 = new SHA1Managed();
+                hash = sha1.ComputeHash(messageByte);
+            }
+            else if (hashAlgorithm == HashAlgorithm.SHA256)
+            {
+                SHA256Managed sha256 = new SHA256Managed();
+                hash = sha256.ComputeHash(messageByte);
+            }
+
+            /// Use RSACryptoServiceProvider support to compare two - hash value from signature and newly created hash value
+            return csp.VerifyHash(hash, CryptoConfig.MapNameToOID(hashAlgorithm.ToString()), signature);
+        }
+	}
+}
