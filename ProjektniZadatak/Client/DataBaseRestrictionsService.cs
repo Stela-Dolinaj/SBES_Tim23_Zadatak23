3b5bdf4cf923b5b2dcea49dfb2741cd2f04ff34a
Igor Jelic <igor.jelic.98@gmail.com>
2021-12-21 12:31:18 +0100

    Added IDatabaseRestrictions interface for handling clients communication. Class DataBaseRestrictionsService implements interface on client side.

---
+++ ProjektniZadatak/Client/DataBaseRestrictionsService.cs
@@ -0,0 +1,22 @@
+﻿using Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    public class DataBaseRestrictionsService : IDatabaseRestrictions
+    {
+        public void NotifyConnectionIsClosed()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void NotifyConnectionIsOpen()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
