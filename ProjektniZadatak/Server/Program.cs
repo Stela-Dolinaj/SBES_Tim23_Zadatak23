3ea3fab4401b9f6963cdacad161ea295a2db38e9
Igor Jelic <igor.jelic.98@gmail.com>
2021-12-29 00:30:32 +0100

    Servis pokrenut uspesno

--- ProjektniZadatak/Server/Program.cs
+++ ProjektniZadatak/Server/Program.cs
@@ -56,13 +56,49 @@ namespace Server
             */
             #endregion
 
-            using (ServiceHost serviceHost = 
-                new ServiceHost(typeof(DatabaseHandlingService)))
-            {
-                serviceHost.Open();
+            // Ime servisnog korisnika
+            string serviceCertCN =
+                Formatter.ParseName(WindowsIdentity.GetCurrent().Name);
 
-                Console.WriteLine("DataBase Service Opened @" + DateTime.Now.ToLongTimeString());
+            NetTcpBinding binding = new NetTcpBinding();
+            binding.Security.Transport.ClientCredentialType =
+                TcpClientCredentialType.Certificate;
+
+            string hostAddressC2DB = "net.tcp://localhost:8080/DbService";
+            string hostAddressC2C = "net.tcp://localhost:8090/ClientCommunication";
+
+            ServiceHost host = new ServiceHost(typeof(DatabaseHandlingService));
+
+            // endpoint za komunikaciju sa bazom podataka
+            host.AddServiceEndpoint(typeof(IDatabaseHandling), binding, hostAddressC2DB);
+            // endpoint za medjuklijentsku komunikaciju
+            host.AddServiceEndpoint(typeof(IClientCommunications), binding, hostAddressC2C);
+
+            // Trust Chain validacija
+            host.Credentials.ClientCertificate.Authentication.CertificateValidationMode =
+                X509CertificateValidationMode.ChainTrust;
+            host.Credentials.ClientCertificate.Authentication.RevocationMode =
+                X509RevocationMode.NoCheck;
+
+            // Postavljanje servisnog sertifikata
+            host.Credentials.ServiceCertificate.Certificate =
+                CertManager.GetCertificateFromStorage(StoreName.My, StoreLocation.LocalMachine, serviceCertCN);
+
+            try
+            {
+                host.Open();
+
+                Console.WriteLine(">> DbHandling Service is running...\nPress <enter> to stop ...");
                 Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("[ERROR] " + e.Message);
+                Console.WriteLine("[StackTrace] " + e.StackTrace);
+            }
+            finally
+            {
+                host.Close();
             }
         }
     }
